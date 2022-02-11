18b8e6a54a8fecfc349693b33e5b5aa431154f89
adam.arlotte <arlotte@hotmail.co.uk>
2022-02-11 17:36:44 +0000

    16 mins adding JS

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const queryParamsContainer = document.querySelector('[data-query-parmams]')
-const requestHeadersContainer = document.querySelector('[data-request-headers]')
+const queryParamsContainer = document.querySelector('[data-query-params]');
+const requestHeadersContainer = document.querySelector('[data-request-headers]');
 
 
-const keyValueTemplate = document.querySelector('[data-key-value-templates]')
+const keyValueTemplate = document.querySelector('[data-key-value-template]');
+
+document.querySelector('[data-add-query-params-btn]').addEventListener('click', () => {
+    queryParamsContainer.append(createKeyValuePair());
+});
 
 
-queryParamsContainer.appendChild(createKeyValuePair());
-requestHeadersContainer.appendChild(createKeyValuePair());
+
+queryParamsContainer.append(createKeyValuePair());
+requestHeadersContainer.append(createKeyValuePair());
 
 
 
 function createKeyValuePair() {
-    const elem = keyValueTemplate.textContent.cloneNode(true);
+    const elem = keyValueTemplate.content.cloneNode(true);
+    elem.querySelector("[data-remove-btn]").addEventListener('click', (e) => {
+        e.target.closest('[data-key-value-pair]').remove();
+    });
+    return elem;
+
 }
