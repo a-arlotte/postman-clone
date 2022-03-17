a860adef8cf3d0cab43ef9733d6e9ffb2210e547
adam.arlotte <adam.arlotte@acftechnologies.com>
2022-03-17 16:05:30 +0000

    up to 38 mins

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ### Following Tutorial
 
 Web Dev Simplified - Stop Using Postman
+
+
+### For Testing
+
+use https://jsonplaceholder.typicode.com/todos/1
--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import prettyBytes from 'pretty-bytes';
+import setupEditor from './setupEditor';
 
+
+const form = document.querySelector("[data-form]");
 
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
@@ -8,14 +13,60 @@ const requestHeadersContainer = document.querySelector('[data-request-headers]')
 
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
 
+
+const responseHeadersContainer = document.querySelector("[data-response-headers]");
+
 document.querySelector('[data-add-query-params-btn]').addEventListener('click', () => {
     queryParamsContainer.append(createKeyValuePair());
 });
 
 
 
+
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
+
+
+axios.interceptors.request.use(request => {
+    request.customData = request.customData || {};
+    request.customData.startTime = new Date().getTime();
+    return request;
+});
+
+axios.interceptors.response.use(updateEndTime, e => {
+    return Promise.reject(updateEndTime(e.response));
+});
+
+function updateEndTime(response) {
+    response.customData = response.customData || {};
+    response.customData.time = new Date().getTime() - response.config.customData.startTime;
+    return response;
+}
+
+
+const {
+    requestEditor,
+    updateResponseEditor
+} = setupEditor();
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    axios({
+        url: document.querySelector("[data-url]").value,
+        method: document.querySelector("[data-method]").value,
+        params: kvpToObj(queryParamsContainer),
+        headers: kvpToObj(requestHeadersContainer)
+
+    }).catch(e => e).then(response => {
+        document.querySelector("[data-response-section]").classList.remove("d-none");
+        updateResponseDetails(response);
+        updateResponseEditor(reponse.data);
+        updateResponseHeaders(response.headers);
+        console.log(response);
+    });
+})
+
 
 
 
@@ -26,4 +77,40 @@ function createKeyValuePair() {
     });
     return elem;
 
+}
+
+function kvpToObj(container) {
+    const pairs = container.querySelectorAll("[data-key-value-pair]");
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector("[data-key]").value;
+        const value = pair.querySelector("[data-value]").value;
+
+        if (key === '') return data;
+
+        return {
+            ...data,
+            [key]: value
+        }
+    }, {});
+}
+
+
+function updateResponseHeaders(headers) {
+    responseHeadersContainer.innerHtml = "";
+    Object.entries(headers).forEach(([key, value]) => {
+        updateContainer(key);
+        updateContainer(value);
+    });
+}
+
+function updateResponseDetails(response) {
+    document.querySelector("[data-status]").textContent = response.status;
+    document.querySelector("[data-time]").textContent = response.customData.time;
+    document.querySelector("[data-size]").textContent = prettyBytes(JSON.stringify(response.data).length + JSON.stringify(response.headers).length);
+}
+
+function updateContainer(value) {
+    const elem = document.createElement("div");
+    elem.textContent = value;
+    responseHeadersContainer.append(elem);
 }
